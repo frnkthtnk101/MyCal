d87946703ab83736f1b2040b05a9b5af10b953c4
frnkthtnk101 <c0demonkers@gmail.com>
2021-05-09 09:00:17 -0400

    Update MyCal.py

--- MyCal.py
+++ MyCal.py
@@ -4,6 +4,27 @@ Create a professional looking calendars without spending millions of dollars.
 '''
 import argparse
 import os
+from PIL import Image, ImageDraw, ImageFont
+
+
+class Image_generator_interface:
+    def create_image(self, picture, font, destination, text):
+        """Create an image and save."""
+        pass
+
+
+class Picture_on_top_with_Text_at_bottom(Image_generator_interface):
+    def create_image(self, picture, font, destination, text):
+        given_image = Image.open(picture)
+        picture_size = (given_image.size[0] * 1.25, given_image.size[1] * 1.25)
+        generated_image = Image.new('RGB', picture_size, color='white')
+        offset_for_image = (picture.size[0] * .25, picture.size[1] * .10)
+        generated_image.paste(given_image,offset_for_image)
+        fnt = ImageFont.truetype(font, 15)
+        d = ImageDraw.Draw(generated_image)
+        d.text((10, 10), text, font=fnt, fill=(0, 0, 0))
+        generated_image.save(destination)
+
 
 
 class Args:
@@ -14,8 +35,13 @@ class Args:
 
 
 def main(args):
-    print(args.font)
+    img = Image.new('RGB', (100, 100), color='white')
+    fnt = ImageFont.truetype(args.font, 15)
+    d = ImageDraw.Draw(img)
+    d.text((10, 10), "I <3 bruna.", font=fnt, fill=(0, 0, 0))
 
+
+    img.save(args.destination)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,7 +56,7 @@ if __name__ == '__main__':
         if font_given:
             font = given_args.font
         else:
-            font = "Comic Sans"
+            font = "C:\Windows\Fonts\comic.ttf"
         destination_directory_given = given_args.output is not None
         if destination_directory_given:
             destination = given_args.output
