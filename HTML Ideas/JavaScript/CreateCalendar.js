e2779db94b06b14e5b36bf19c6feaf914fddb40e
frnkthtnk101 <c0demonkers@gmail.com>
2021-05-16 09:08:39 -0400

    have the calendar the way I want it

--- HTML Ideas/JavaScript/CreateCalendar.js
+++ HTML Ideas/JavaScript/CreateCalendar.js
@@ -1,15 +1,11 @@
 var TheMonths = ["January", "Feburary", "March", "April","May", "June", "July", "August", "September", "October", "November", "Decemeber"];
 var TodaysDate = new Date();
-
 var ThisMonth = TodaysDate.getMonth();
 var  firstOfTheMonth = new Date(TodaysDate.getFullYear(),ThisMonth,1);
 var UtcDay = firstOfTheMonth.getDay();
-
 //Make the month tag correct
 var monthPlaceHolder = document.getElementById("MonthName");
 monthPlaceHolder.innerText = TheMonths[ThisMonth] + ' ' + firstOfTheMonth.getFullYear();
-
-//var DaysInCalendar = new Array(UtcDay).fill(0);
 var CurrentMonth = ThisMonth;
 var DaysInCalendar = new Array();
 while(UtcDay > 0)
@@ -19,23 +15,15 @@ while(UtcDay > 0)
     DaysInCalendar.push(pastDate.getDate());
     UtcDay -=1
 } 
-/*  
-do{
-    DaysInCalendar.push(firstOfTheMonth.getDate());
-    firstOfTheMonth.setDate(firstOfTheMonth.getDate() + 1);
-    CurrentMonth =  firstOfTheMonth.getMonth();
-}while(CurrentMonth == ThisMonth);
-while(firstOfTheMonth.getDay() < 6){
-    DaysInCalendar.push(firstOfTheMonth.getDate());
-    firstOfTheMonth.setDate(firstOfTheMonth.getDate() + 1);
-}*/
 var index = 0;
 var lenghtOfArray = DaysInCalendar.length;
 var calendar = document.getElementById("CalTable");
+var lastNumber = 0;
 for(i = 0; i < 6; i+=1){
     var row = calendar.insertRow(i+1);
     for(j = 0; j < 7; j +=1 )
     {
+        var className = "otherMonths";
         var cell = row.insertCell(j);
         if(index < lenghtOfArray)
         {
@@ -44,8 +32,14 @@ for(i = 0; i < 6; i+=1){
         }
         else
         {
-            cell.innerText = firstOfTheMonth.getDate();
+            var currentDate = firstOfTheMonth.getDate();
+            if(currentDate > lastNumber){
+                var className = "currentMonth";
+                lastNumber = currentDate;
+            }
+            cell.innerText = currentDate;
             firstOfTheMonth.setDate(firstOfTheMonth.getDate() + 1);
         }
+        cell.className += className;
     }
 }
